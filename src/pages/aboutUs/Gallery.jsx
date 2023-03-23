3cf3a3c3c18d5851d989c775d5dd94e5e0a5ae5e
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2023-03-24 02:46:56 +0700

    gallery

--- src/pages/aboutUs/Gallery.jsx
+++ src/pages/aboutUs/Gallery.jsx
@@ -22,7 +22,68 @@ export default function Gallery() {
       <Header />
 
       <div className="container">
-        <section className="my-5" data-aos="fade-up"></section>
+        <section className="my-5" data-aos="fade-up">
+          <h2 className="text-center">Gallery</h2>
+          <br />
+          <div className="d-grid gap-3">
+            <div>
+              <iframe
+                src="https://drive.google.com/file/d/1M-fehIcbdrbdpKeXGrwyvU0vzLmw-OJy/preview"
+                className="rounded shadow-sm w-100"
+                title="Video"
+                allow="autoplay"
+                allowFullScreen="true"
+                height={560}
+              ></iframe>
+            </div>
+            <div>
+              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/5.jpg"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/6.jpg"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/7.png"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/8.png"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/9.png"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+                <div className="col">
+                  <img
+                    src="images/about_us/gallery/10.png"
+                    alt="Foto"
+                    className="rounded shadow-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
 
       <Footer />
